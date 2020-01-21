7681bc8133547d1039ffcab157c558d796defd04
brickatyourfeet <brickatyourfeet@gmail.com>
2020-01-20 21:29:24 -0800

    Initial Commit

---
+++ Count By/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count By
+//
+//  Created by Kyle Braden on 1/20/20.
+//  Copyright © 2020 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
